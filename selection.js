ef608ec22be8d85b8a46548c1371e0a74bb82d68
unknown <andrew@elementalsystems.co.za>
2014-12-14 22:27:50 +0200

    more css

--- selection.js
+++ selection.js
@@ -1,13 +1,20 @@
 
 var game={};
+//game status values
+// 0 - starting round
+// 1 - end play
+// 2 - end of round
 
 function start()
 {
+  
+  game.holder=document.getElementById('game');
   game.board=document.getElementById('board');
   game.scoreSpan=document.getElementById('score');
   game.foundCountSpan=document.getElementById('foundcount');
   game.totalCountSpan=document.getElementById('totalcount');
   game.timeLeftSpan=document.getElementById('timeleft');  
+  game.timePrompt=document.getElementById('timeprompt');  
   game.oneRuleSpan=document.getElementById('onerule');    
   game.score=0;
   createLevel('lev1'); 
@@ -18,7 +25,14 @@ function setScoreboard()
   game.scoreSpan.innerHTML=Number(game.score);
   game.foundCountSpan.innerHTML=Number(game.foundCount);
   game.totalCountSpan.innerHTML=Number(game.totalCount);
-  game.timeLeftSpan.innerHTML=Number((game.timeEnd-game.timeNow)/1000);    
+  if (game.status==0) {
+    game.timePrompt.innerHTML="Starting in...";
+    game.timeLeftSpan.innerHTML=Number(Math.round((game.timeStart-game.timeNow)/1000));    
+  }
+  if (game.status==1) {
+    game.timePrompt.innerHTML="Time Left";
+    game.timeLeftSpan.innerHTML=Number(Math.round((game.timeEnd-game.timeNow)/1000));    
+  }  
 }
 
 
@@ -39,8 +53,26 @@ function timerTick()
 {
   game.timeNow=new Date().getTime();
   //check for status
+  if (game.status==0)  { //starting up
+    if (game.timeNow>game.timeStart) { //start of round
+	    if (!game.holder.classList.contains('inplay'))
+		  game.holder.classList.add('inplay');
+		if (game.holder.classList.contains('roundover'))
+		  game.holder.classList.remove('roundover');
+		game.status=1; //inplay now
+     }
+   }
+   else if (game.status==1) {
+     if (game.timeNow>game.timeEnd) { //start of round
+	    if (game.holder.classList.contains('inplay'))
+		  game.holder.classList.remove('inplay');
+		if (!game.holder.classList.contains('roundover'))
+		  game.holder.classList.add('roundover');		  
+		game.status=2;//game ended
+     }
+  }
   setScoreboard();
-  setTimeout(timerTick,250);
+  setTimeout(timerTick,500);    
 }
 
 function createLevel(level)
@@ -54,8 +86,8 @@ function createLevel(level)
    game.foundCount=0;
    game.timeInit=new Date().getTime();
    game.timeStart=game.timeInit+5000;
-   game.timeEnd=game.timeStart+30000;
-   game.inPlay=false;
+   game.timeEnd=game.timeStart+20000;
+   game.status=0;
    
    for (var i=0;i<items.length;i+=1) {
        var item=items[i];
